58438b5fef558d094dec5f73de53a3001ed176c1
Matilde Lombardo <lombardomatilde11@gmail.com>
2023-04-03 18:47:12 +0200

    Corrected the check in the class GoalC4 related to the common objective C4

--- src/main/java/it/polimi/ingsw/commonStrategy/GoalC4.java
+++ src/main/java/it/polimi/ingsw/commonStrategy/GoalC4.java
@@ -12,9 +12,9 @@ public class GoalC4 {
             int count = 0;
 
             for(int j = 0; j<5 ; j++){//columns
-                for(int i=0; i<5; i++){//rows, <5 because the 6th row is the last one and can't compare it to the tiles down
+                for(int i=0; i<6; i++){//rows
                     type = p.getMy_shelfie().getShelf()[i][j].getType();
-                    if ((p.getMy_shelfie().getShelf()[i++][j].getType()) == type)
+                    if (((p.getMy_shelfie().getShelf()[i+1][j].getType()) == type) || ((p.getMy_shelfie().getShelf()[i][j-1].getType()) == type) ||  ((p.getMy_shelfie().getShelf()[i][j+1].getType()) == type) )
                         count++;
                     if (count < 6)
                         continue;

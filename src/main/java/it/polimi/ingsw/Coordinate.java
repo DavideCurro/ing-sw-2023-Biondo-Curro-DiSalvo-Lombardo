72dbc5dd2fb01a2f37dd44fd09973c2376c452f5
Claudio Di Salvo <claudio.disalvo@mail.polimi.it>
2023-04-07 11:37:44 +0200

    Coordinate update for personalobj

--- src/main/java/it/polimi/ingsw/Coordinate.java
+++ src/main/java/it/polimi/ingsw/Coordinate.java
@@ -5,17 +5,28 @@ import java.util.*;
 public class Coordinate {
     Vector<Integer> X;
     Vector<Integer> Y;
-
+    Vector<Integer> type;
     public Coordinate() {
         this.X = new Vector<Integer>();
         this.Y = new Vector<Integer>();
+        this.type = new Vector<Integer>()();
 
     }
 
-    public Coordinate(Vector<Integer> x, Vector<Integer> y) {
-        X = x;
-        Y = y;
+    public Coordinate(Vector<Integer> x, Vector<Integer> y,Vector<Integer> type ) {
+        this.X = x;
+        this.Y = y;
+        this.type = type;
     }
+
+    public Vector<Integer> getType() {
+        return type;
+    }
+
+    public void setType(Vector<Integer> type) {
+        this.type = type;
+    }
+
     public Vector<Integer> getX() {
         return X;
     }
@@ -42,4 +53,12 @@ public class Coordinate {
     public Integer size(){
         return Math.max(X.size(), Y.size());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Coordinate that)) return false;
+        return Objects.equals(type, that.type);
+    }
+
 }

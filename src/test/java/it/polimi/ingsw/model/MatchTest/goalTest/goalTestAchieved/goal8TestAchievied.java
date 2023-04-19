96fa98237a086957329c3d262bad3ce334d4e045
DavideCurro <davide2.curro@mail.polimi.it>
2023-04-19 18:48:42 +0200

    added test GoalC8

--- src/test/java/it/polimi/ingsw/model/MatchTest/goalTest/goalTestAchieved/goal8TestAchievied.java
+++ src/test/java/it/polimi/ingsw/model/MatchTest/goalTest/goalTestAchieved/goal8TestAchievied.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.model.MatchTest.goalTest.goalTestAchieved;
 
 import it.polimi.ingsw.model.Playground.Tiles;
-import it.polimi.ingsw.model.commonStrategy.GoalC2;
+import it.polimi.ingsw.model.commonStrategy.GoalC8;
 import it.polimi.ingsw.model.commonStrategy.ObjectiveCommonEXEC;
 import it.polimi.ingsw.model.player.Library;
 import it.polimi.ingsw.model.player.Player;
@@ -13,18 +13,23 @@ import java.util.Random;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class goal2TestAchieved {
-    private ObjectiveCommonEXEC objectiveCommonEXEC = new ObjectiveCommonEXEC(new GoalC2());
+public class goal8TestAchievied {
+    private ObjectiveCommonEXEC objectiveCommonEXEC = new ObjectiveCommonEXEC(new GoalC8());
     private Library library = new Library();
+
     @Before
     public void setUp(){
 
         for(int i = 0; i<6;i++){
             for(int j = 0; j <5; j++){
-                library.getShelf()[i][j]=new Tiles(i);
+                library.getShelf()[i][j]=new Tiles(-1);
             }
-
         }
+        library.getShelf()[0][0].setType(5);
+        library.getShelf()[5][0].setType(5);
+
+        library.getShelf()[0][4].setType(5);
+        library.getShelf()[5][4].setType(5);
 
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 5; j++) {
--- src/test/java/it/polimi/ingsw/model/MatchTest/goalTest/goalTestNotAchieved/goal8TestNotAchieved.java
+++ src/test/java/it/polimi/ingsw/model/MatchTest/goalTest/goalTestNotAchieved/goal8TestNotAchieved.java
@@ -1,30 +1,33 @@
-package it.polimi.ingsw.model.MatchTest.goalTest.goalTestAchieved;
+package it.polimi.ingsw.model.MatchTest.goalTest.goalTestNotAchieved;
 
 import it.polimi.ingsw.model.Playground.Tiles;
-import it.polimi.ingsw.model.commonStrategy.GoalC2;
+import it.polimi.ingsw.model.commonStrategy.GoalC8;
 import it.polimi.ingsw.model.commonStrategy.ObjectiveCommonEXEC;
 import it.polimi.ingsw.model.player.Library;
 import it.polimi.ingsw.model.player.Player;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Random;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class goal2TestAchieved {
-    private ObjectiveCommonEXEC objectiveCommonEXEC = new ObjectiveCommonEXEC(new GoalC2());
+public class goal8TestNotAchieved {
+    private ObjectiveCommonEXEC objectiveCommonEXEC = new ObjectiveCommonEXEC(new GoalC8());
     private Library library = new Library();
+
     @Before
     public void setUp(){
 
         for(int i = 0; i<6;i++){
             for(int j = 0; j <5; j++){
-                library.getShelf()[i][j]=new Tiles(i);
+                library.getShelf()[i][j]=new Tiles(-1);
             }
-
         }
+        library.getShelf()[2][0].setType(5);
+        library.getShelf()[5][0].setType(5);
+
+        library.getShelf()[0][4].setType(5);
+        library.getShelf()[5][4].setType(5);
 
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 5; j++) {
@@ -36,8 +39,6 @@ public class goal2TestAchieved {
     }
     @Test
     public void validateGoal1(){
-        assertTrue(objectiveCommonEXEC.execCheck(new Player(library)));
+        assertFalse(objectiveCommonEXEC.execCheck(new Player(library)));
     }
-
-
 }

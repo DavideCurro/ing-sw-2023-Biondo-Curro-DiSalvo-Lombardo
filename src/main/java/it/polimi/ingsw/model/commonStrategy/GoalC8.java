870986a536c9f1db5a6905c4f25d2ae39b21c7cf
DavideCurro <davide2.curro@mail.polimi.it>
2023-04-19 18:49:47 +0200

    fixed GoalC8

--- src/main/java/it/polimi/ingsw/model/commonStrategy/GoalC8.java
+++ src/main/java/it/polimi/ingsw/model/commonStrategy/GoalC8.java
@@ -6,10 +6,7 @@ import it.polimi.ingsw.model.player.Player;
            corners of the bookshelf.*/
 public class GoalC8 implements CommonObj {
     public boolean check(Player p) {
-       /* for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 5; j++) {
-            }
-        }*/
+
         if (p.getMy_shelfie().getShelf()[0][0].getType() == p.getMy_shelfie().getShelf()[0][4].getType() && p.getMy_shelfie().getShelf()[0][4].getType() == p.getMy_shelfie().getShelf()[5][4].getType()
                 && p.getMy_shelfie().getShelf()[5][0].getType() == p.getMy_shelfie().getShelf()[5][4].getType() && p.getMy_shelfie().getShelf()[0][0].getType() == p.getMy_shelfie().getShelf()[0][4].getType()) {
             System.out.println("Goal completed");
@@ -18,29 +15,7 @@ public class GoalC8 implements CommonObj {
             System.out.println("Goal not completed");
             return false;
         }
-
-  /*              //check on first column
-                if (p.getMy_shelfie().getShelf()[0][0].getType() == p.getMy_shelfie().getShelf()[0][4].getType()) {
-                    System.out.println("The Tiles are equal");
-                } else return true;
-                //check on last column
-                if (p.getMy_shelfie().getShelf()[0][4].getType() == p.getMy_shelfie().getShelf()[5][4].getType()) {
-                    System.out.println("The Tiles are equal");
-                } else return true;
-                //check if the bottom corners are equals
-                if (p.getMy_shelfie().getShelf()[5][0].getType() == p.getMy_shelfie().getShelf()[5][4].getType()) {
-                    System.out.println("The Tiles are equal");
-                } else return true;
-                ;
-                //check if the top corners are equal
-                if (p.getMy_shelfie().getShelf()[0][0].getType() == p.getMy_shelfie().getShelf()[0][4].getType()) {
-                    System.out.println("The Tiles are equal");
-                } else return true;
-
-        System.out.println("Objective completed");
-        return false;
-
-    }*/
     }
+
 }
 

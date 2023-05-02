1570d22a72ea84d6332104608591452ef8cb7cfe
DavideCurro <davide2.curro@mail.polimi.it>
2023-05-02 21:16:51 +0200

    little fix in setupController

--- src/main/java/it/polimi/ingsw/socket/client/GUI/Controllers/setupController.java
+++ src/main/java/it/polimi/ingsw/socket/client/GUI/Controllers/setupController.java
@@ -1,13 +1,34 @@
 package it.polimi.ingsw.socket.client.GUI.Controllers;
 
+import it.polimi.ingsw.socket.server.GameHandler;
+import it.polimi.ingsw.socket.server.StarterServer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.util.Scanner;
 
 public class setupController {
     @FXML
     Button startGame;
-    public void startGame(MouseEvent mouseEvent) {
-        startGame.setText("Benvenuto");
+    @FXML
+    Label selectNickname;
+    @FXML
+    TextField writeNickname;
+
+
+    public void initialize() {
+        startGame.setVisible(false);
+        selectNickname.setVisible(true);
+        writeNickname.setVisible(true);
     }
+
+    public void startGame(MouseEvent mouseEvent) {
+        startGame.setVisible(true);
+    }
+
 }

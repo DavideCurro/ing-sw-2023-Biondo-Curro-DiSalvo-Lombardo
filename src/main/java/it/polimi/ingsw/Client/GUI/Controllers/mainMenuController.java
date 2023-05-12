93461686c4e3e790f2e5d83f61ae48486c852313
Sibb00 <104092700+Sibb00@users.noreply.github.com>
2023-05-12 18:59:12 +0200

    added possibility to take a tile from playground and put in the library

--- src/main/java/it/polimi/ingsw/Client/GUI/Controllers/mainMenuController.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/Controllers/mainMenuController.java
@@ -1,10 +1,14 @@
 package it.polimi.ingsw.Client.GUI.Controllers;
 
+import it.polimi.ingsw.Client.MessageDispatcher;
+import it.polimi.ingsw.Model.Playground.Tiles;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+
+import java.util.Vector;
 
 public class mainMenuController {
     @FXML
@@ -19,17 +23,42 @@ public class mainMenuController {
     ImageView commonG1;
     @FXML
     GridPane library;
+    private MessageDispatcher message;
 
+    public mainMenuController(MessageDispatcher message) {
+        this.message = message;
+    }
 
     /**
      *
      * @param
      */
-    public void insertTiles(MouseEvent mouseEvent) {
+
+    //pick tiles from playground
+    public void pickTiles(MouseEvent mouseEvent) {
+        Vector<Tiles> tmp = new Vector<>();
+
+        int x1 = (int) (playground.getLayoutX());
+        int y1 = (int) (playground.getLayoutY());
+
+        tmp.add(new Tiles(-1,x1,y1));
+
+        Button button = new Button("Confirm");
+        button.setOnAction(e -> insertTiles(tmp));
+
+        //objectOutput.write(new Message("io","tu", Content.PICKEDTILE,column,tmp));
+
+    }
+    
+    public void insertTiles( Vector<Tiles> t) {
+        library.setOnMouseClicked(e-> {
+            int column = (int) library.getLayoutY();
+            message.sendPickUpData(t,column);
+
+        });
 
     }
 
-    public void pickTiles(MouseEvent mouseEvent) {
-
+    public void insertTiles(MouseEvent mouseEvent) {
     }
 }

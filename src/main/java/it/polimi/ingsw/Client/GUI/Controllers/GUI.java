6b87e54d8eb693314193df7ba6451d19f39cf6cb
Matilde Lombardo <lombardomatilde11@gmail.com>
2023-05-13 10:59:50 +0200

    Implementation of the insertion of the tiles in the library in GUI

--- src/main/java/it/polimi/ingsw/Client/GUI/Controllers/GUI.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/Controllers/GUI.java
@@ -3,20 +3,26 @@ package it.polimi.ingsw.Client.GUI.Controllers;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GUI extends Application {
 
+    Button but1;
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("/it/polimi/MyShelfie/setup.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Title");
+        but1 = new Button();
+        but1.setText("click");
+        but1.setOnAction(e -> System.out.println("cliccato"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/it/polimi/ingsw/Client/GUI/Controllers/mainMenuController.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/Controllers/mainMenuController.java
@@ -24,8 +24,16 @@ public class mainMenuController {
     @FXML
     GridPane library;
     private MessageDispatcher message;
+    private Vector<Tiles> tmp;
 
+    private int column = 0;
     public mainMenuController(MessageDispatcher message) {
+        otherPlayers.setVisible(true);
+        personalCard.setVisible(true);
+        commonG1.setVisible(true);
+        commonG2.setVisible(true);
+        playground.setVisible(true);
+        library.setVisible(true);
         this.message = message;
     }
 
@@ -36,29 +44,57 @@ public class mainMenuController {
 
     //pick tiles from playground
     public void pickTiles(MouseEvent mouseEvent) {
-        Vector<Tiles> tmp = new Vector<>();
+        tmp = new Vector<>();
+
+        Button button = new Button("Confirm");
+        button.setVisible(false);
 
         int x1 = (int) (playground.getLayoutX());
         int y1 = (int) (playground.getLayoutY());
 
         tmp.add(new Tiles(-1,x1,y1));
+        button.setVisible(true);
 
-        Button button = new Button("Confirm");
-        button.setOnAction(e -> insertTiles(tmp));
+        //buttons to choose the column where to enter the tiles in the library
+        Button col0 = new Button("1");
+        Button col1 = new Button("2");
+        Button col2 = new Button("3");
+        Button col3 = new Button("4");
+        Button col4 = new Button("5");
+        col0.setVisible(false);
+        col1.setVisible(false);
+        col2.setVisible(false);
+        col3.setVisible(false);
+        col4.setVisible(false);
 
-        //objectOutput.write(new Message("io","tu", Content.PICKEDTILE,column,tmp));
+        button.setOnAction(e ->{
+            col0.setVisible(true);
+            col1.setVisible(true);
+            col2.setVisible(true);
+            col3.setVisible(true);
+            col4.setVisible(true);
+        } );
 
-    }
-    
-    public void insertTiles( Vector<Tiles> t) {
-        library.setOnMouseClicked(e-> {
-            int column = (int) library.getLayoutY();
-            message.sendPickUpData(t,column);
-
+        col0.setOnAction(e -> {
+            column = 0;
+            message.sendPickUpData(tmp,column);
+        });
+        col1.setOnAction(e ->{
+            column = 1;
+            message.sendPickUpData(tmp,column);
+        });
+        col2.setOnAction(e -> {
+            column = 2;
+            message.sendPickUpData(tmp,column);
+        });
+        col3.setOnAction(e -> {
+            column = 3;
+            message.sendPickUpData(tmp,column);
+        });
+        col4.setOnAction(e -> {
+            column = 4;
+            message.sendPickUpData(tmp,column);
         });
 
-    }
-
-    public void insertTiles(MouseEvent mouseEvent) {
     }
 }

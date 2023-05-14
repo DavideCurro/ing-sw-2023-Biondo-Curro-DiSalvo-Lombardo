4b95d9750f6c7d6a46ec6b1d8edfcfce33729fe3
DavideCurro <davide2.curro@mail.polimi.it>
2023-05-14 17:16:47 +0200

    trying to fix problems with Gui

--- src/main/java/it/polimi/ingsw/Client/GUI/Controllers/GUI.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/Controllers/GUI.java
@@ -13,14 +13,20 @@ public class GUI extends Application {
     Button but1;
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("it/polimi/ingsw/Client/GUI/Controllers/setup.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        primaryStage.setTitle("Title");
-        but1 = new Button();
-        but1.setText("click the button");
-        but1.setOnAction(e -> System.out.println("cliccato"));
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("resources/it.polimi.MyShelfie/fxml/setup.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            primaryStage.setTitle("Title");
+            but1 = new Button();
+            but1.setText("click the button");
+            but1.setOnAction(e -> System.out.println("cliccato"));
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+
     }
 
 

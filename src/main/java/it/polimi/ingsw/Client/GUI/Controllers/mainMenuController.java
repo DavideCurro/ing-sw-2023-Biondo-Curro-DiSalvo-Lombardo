ac3be4df9142df5ad4a04a8b2ee1bd98a7926649
DavideCurro <davide2.curro@mail.polimi.it>
2023-05-18 16:20:55 +0200

    fixed method for click on gridpane

--- src/main/java/it/polimi/ingsw/Client/GUI/Controllers/mainMenuController.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/Controllers/mainMenuController.java
@@ -37,9 +37,6 @@ public class mainMenuController {
 
     private int column = 0;
 
-    private int columnp;
-    private int rowsp;
-
     private final int TILE_WIDTH = 52;
     private final int TILE_HEIGHT = 52;
 
@@ -54,7 +51,7 @@ public class mainMenuController {
 
     }
 
-    public void initialize(){
+    public void initialize() {
         otherPlayers.setVisible(true);
         personalCard.setVisible(true);
         commonG1.setVisible(true);
@@ -74,38 +71,43 @@ public class mainMenuController {
 
     /**
      * Prints the playground everytime the server sends it to the client
+     *
      * @param playgroundmodel
      */
-    public void printplaygroundBoard(Playground playgroundmodel){
+    public void printplaygroundBoard(Playground playgroundmodel) {
 
         // playground.setStyle("-fx-background-image : url ");
 
         initTilesimage();
 
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; i < 9; j++){
-               int index =  playgroundmodel.getGround()[i][j].getType();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; i < 9; j++) {
+                int index = playgroundmodel.getGround()[i][j].getType();
 
-               ImageView img = new ImageView(tilesImg.get(index));
-               img.setFitHeight((TILE_HEIGHT));
-               img.setFitWidth((TILE_WIDTH));
-               playground.getChildren().add(img);
+                ImageView img = new ImageView(tilesImg.get(index));
+                img.setFitHeight((TILE_HEIGHT));
+                img.setFitWidth((TILE_WIDTH));
+                playground.getChildren().add(img);
             }
         }
     }
 
     /**
      * Prints the personal goal of the player
+     *
      * @param playermodel
      */
-    public void printPersonalGoal(Player playermodel){
+    public void printPersonalGoal(Player playermodel) {
+    }
+
+    public void printCommonGoal(Player playermodel) {
 
     }
 
     /**
      * Method that associate the image of the tile to the tile itself
      */
-    public void initTilesimage(){
+    public void initTilesimage() {
         tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Cornici1.1.png").toExternalForm()));
         tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Trofei1.1.png").toExternalForm()));
         tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Giochi1.1.png").toExternalForm()));
@@ -113,73 +115,91 @@ public class mainMenuController {
         tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Gatti1.1.png").toExternalForm()));
         tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Piante1.1.png").toExternalForm()));
 
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Cornici1.2.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Trofei1.2.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Giochi1.2.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Libri1.2.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Gatti1.2.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Piante1.2png").toExternalForm()));
+
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Cornici1.3.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Trofei1.3.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Giochi1.3.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Libri1.3.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Gatti1.3.png").toExternalForm()));
+        tilesImg.add(new Image(this.getClass().getResource("/resouces/item_tiles/Piante1.3.png").toExternalForm()));
 
     }
 
     /**
      * pick tiles from playground
      */
-    public void pickTiles(MouseEvent mouseEvent, Node node) {
+    public void pickTiles(MouseEvent mouseEvent) {
         tmp = new Vector<>();
 
         button.setVisible(true);
 
-        node.setOnMousePressed(mouseEvent1 -> {
-            mouseGridX = mouseEvent1.getX();
-            mouseGridY = mouseEvent1.getY();
+        playground.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+            mouseGridX = e.getX();
+            mouseGridY = e.getY();
+
         });
 
-            tmp.add(new Tiles(-1, (int) mouseGridX, (int) mouseGridY));
+
+        tmp.add(new Tiles(-1, (int) mouseGridX, (int) mouseGridY));
     }
 
     /**
      * Inserts the picked tiles from the playground, in the library of the player, in the column chosen by a button
+     *
      * @param mouseEvent
      */
-    public void insertTiles(MouseEvent mouseEvent){
+    public void insertTiles(MouseEvent mouseEvent) {
 
 
-        button.setOnAction(e ->{
+        button.setOnAction(e -> {
             button0.setVisible(true);
             button1.setVisible(true);
             button2.setVisible(true);
             button3.setVisible(true);
             button4.setVisible(true);
-        } );
+        });
 
         button0.setOnAction(e -> {
             column = 0;
-            message.sendPickUpData(tmp,column);
+            message.sendPickUpData(tmp, column);
         });
-        button1.setOnAction(e ->{
+        button1.setOnAction(e -> {
             column = 1;
-            message.sendPickUpData(tmp,column);
+            message.sendPickUpData(tmp, column);
         });
         button2.setOnAction(e -> {
             column = 2;
-            message.sendPickUpData(tmp,column);
+            message.sendPickUpData(tmp, column);
         });
         button3.setOnAction(e -> {
             column = 3;
-            message.sendPickUpData(tmp,column);
+            message.sendPickUpData(tmp, column);
         });
         button4.setOnAction(e -> {
             column = 4;
-            message.sendPickUpData(tmp,column);
+            message.sendPickUpData(tmp, column);
         });
 
     }
 
     /**
      * Prints the library of a player
+     *
      * @param playermodel
      */
     public void printLibrary(Player playermodel) {
-       otherPlayers.setOnAction(e->{
-           //prints the library of the players
-       });
+        otherPlayers.setOnAction(e -> {
+
+            //prints the library of the players
+        });
 
     }
-
-
 }
+
+

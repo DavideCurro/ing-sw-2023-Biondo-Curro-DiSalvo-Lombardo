b7b2e7f1f6c723ddb9952d2536d5d9c8ec7940fa
Claxl <claxlxl@gmail.com>
2023-06-30 11:47:46 +0200

    Jar File + some fix in commonobj

--- src/main/java/it/polimi/ingsw/Model/CommonStrategy/GoalC12.java
+++ src/main/java/it/polimi/ingsw/Model/CommonStrategy/GoalC12.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.Model.CommonStrategy;
 
 import it.polimi.ingsw.Model.Player.Player;
+import it.polimi.ingsw.Model.Playground.Tiles;
 
 import java.io.Serializable;
+import java.util.Vector;
 
 
 public class GoalC12 implements CommonObj, Serializable {
@@ -10,6 +12,12 @@ public class GoalC12 implements CommonObj, Serializable {
     public int getType(){
         return type;
     }
+    Vector<Tiles> diagBlu = new Vector<>();
+    Vector<Tiles> diagMarr = new Vector<>();
+    Vector<Tiles> diagBianca = new Vector<>();
+    Vector<Tiles> diagVerde = new Vector<>();
+    Vector<Tiles> diagArcobaleno = new Vector<>();
+    Vector<Tiles> diagFucsia = new Vector<>();
 
     /**Five columns of increasing or decreasing
      *  height. Starting from the first column on
@@ -20,75 +28,61 @@ public class GoalC12 implements CommonObj, Serializable {
      * @return
      */
 
-    public boolean check(Player p) {
-        if (leftOrRight(0, p)) {
-            return scalaLeftToRight(0, p);
-        } else if (leftOrRight(4, p)) {
-            return scalaRightToLeft(0, p);
-        } else {
-            return false;
-        }
+    private void setUp(Player p){
+
+        diagBlu.add(new Tiles(p.getMy_shelfie().getShelf()[0][0].getType(), 0, 0));
+        diagBlu.add(new Tiles(p.getMy_shelfie().getShelf()[1][1].getType(), 1, 1));
+        diagBlu.add(new Tiles(p.getMy_shelfie().getShelf()[2][2].getType(), 2, 2));
+        diagBlu.add(new Tiles(p.getMy_shelfie().getShelf()[3][3].getType(), 3, 3));
+        diagBlu.add(new Tiles(p.getMy_shelfie().getShelf()[4][4].getType(), 4, 4));
 
 
+        diagVerde.add(new Tiles(p.getMy_shelfie().getShelf()[0][4].getType(), 0, 4));
+        diagVerde.add(new Tiles(p.getMy_shelfie().getShelf()[1][3].getType(), 1, 3));
+        diagVerde.add(new Tiles(p.getMy_shelfie().getShelf()[2][2].getType(), 2, 2));
+        diagVerde.add(new Tiles(p.getMy_shelfie().getShelf()[3][1].getType(), 3, 1));
+        diagVerde.add(new Tiles(p.getMy_shelfie().getShelf()[4][0].getType(), 4, 0));
+
+
+        diagFucsia.add(new Tiles(p.getMy_shelfie().getShelf()[1][4].getType(), 1, 4));
+        diagFucsia.add(new Tiles(p.getMy_shelfie().getShelf()[2][3].getType(), 2, 3));
+        diagFucsia.add(new Tiles(p.getMy_shelfie().getShelf()[3][2].getType(), 3, 2));
+        diagFucsia.add(new Tiles(p.getMy_shelfie().getShelf()[4][1].getType(), 4, 1));
+        diagFucsia.add(new Tiles(p.getMy_shelfie().getShelf()[5][0].getType(), 5, 0));
+
+        diagMarr.add(new Tiles(p.getMy_shelfie().getShelf()[1][0].getType(), 1, 0));
+        diagMarr.add(new Tiles(p.getMy_shelfie().getShelf()[2][1].getType(), 2, 1));
+        diagMarr.add(new Tiles(p.getMy_shelfie().getShelf()[3][2].getType(), 3, 2));
+        diagMarr.add(new Tiles(p.getMy_shelfie().getShelf()[4][3].getType(), 4, 3));
+        diagMarr.add(new Tiles(p.getMy_shelfie().getShelf()[5][4].getType(), 5, 4));
+
+        diagBianca.add(new Tiles(p.getMy_shelfie().getShelf()[0][1].getType(), 0, 1));
+        diagBianca.add(new Tiles(p.getMy_shelfie().getShelf()[1][2].getType(), 1, 2));
+        diagBianca.add(new Tiles(p.getMy_shelfie().getShelf()[2][3].getType(), 2, 3));
+        diagBianca.add(new Tiles(p.getMy_shelfie().getShelf()[3][4].getType(), 3, 4));
+
+        diagArcobaleno.add(new Tiles(p.getMy_shelfie().getShelf()[0][3].getType(), 0, 3));
+        diagArcobaleno.add(new Tiles(p.getMy_shelfie().getShelf()[1][2].getType(), 1, 2));
+        diagArcobaleno.add(new Tiles(p.getMy_shelfie().getShelf()[2][1].getType(), 2, 1));
+        diagArcobaleno.add(new Tiles(p.getMy_shelfie().getShelf()[3][0].getType(), 3, 0));
     }
 
-    private boolean leftOrRight(int j, Player p) {
-        int count = 0;
-        boolean ok = false;
-        for (int i = 0; i < 6; i++) {
-            if (p.getMy_shelfie().getShelf()[i][j].getType() != -1) {
-                count++;
-            }
-        }
-        if (count == 5 || count == 6) {
-            ok = true;
-        }
-        return ok;
+    public boolean check(Player p){
+        setUp(p);
+        return checkDiagonal(diagBlu, diagBianca) || checkDiagonal(diagMarr,diagBlu) || checkDiagonal(diagVerde,diagArcobaleno) || checkDiagonal(diagFucsia,diagVerde);
     }
 
-    private boolean scalaLeftToRight(int y, Player p) {
-        int offset = 0;
-        if (p.getMy_shelfie().getShelf()[0][0].getType() == -1) {
-            offset = 1;
+    private boolean checkDiagonal(Vector<Tiles> diag1, Vector<Tiles> diag2){
+        for(Tiles tile : diag1){
+            if(!tile.checkSet())
+                return false;
         }
-        for (int j = 0; j < 5; j++) {
-            for (int i = offset; i < 6; i++) {
-                if (p.getMy_shelfie().getShelf()[i][j+1].getType() != -1) return false;
-                while(i == j) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-       /* for (int j = y + 1; j < 5; j++) {
-            for (int i = offset + j; i < 5 - j; i++) {
-
-                if (p.getMy_shelfie().getShelf()[i][j].getType() == -1) {
-                    return false;
-                }
-            }
+        for(Tiles tile : diag2){
+            if(tile.checkSet())
+                return false;
         }
         return true;
-    }*/
-
-    private boolean scalaRightToLeft(int y, Player p) {
-        int offset = 1;
-        if (p.getMy_shelfie().getShelf()[0][4].getType() == -1) {
-            offset = 0;
-        }
-        for (int j = 3; j > 0; j--) {
-            for (int i = offset; i < 6; i++) {
-                if (p.getMy_shelfie().getShelf()[i+1][j].getType() != -1) return false;
-                while (i == j) {
-                    return true;
-                }
-
-                }
-            }
-        return false;
-        }
-
     }
 
+}
 
